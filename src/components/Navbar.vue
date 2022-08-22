3755e8e86d2c9af6fa659684746012ef5b6443d4
Jellinek <jellinekbraus@gmail.com>
2022-08-22 22:19:42 +0500

    day 1

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,4 @@
 <template>
-<<<<<<< HEAD
-    
-=======
     <nav class="header__nav">
         <div class="header__nav-logo">
             <a href="#!"><img src="@/assets/images/logo.png" alt="" class="logo"></a>
@@ -11,18 +8,15 @@
         </ul>
         <div class="header__nav-block">
             <div class="header__nav-translation">
-                <h3 class="header__nav-lang"> 
-                    <span >ru</span>
-                     | 
-                    <span> en</span>
-                    </h3>
+                <h3 class="header__nav-lang" @click="ruActive = true; enActive=false" :class="{active: ruActive}">ru</h3>
+                <p class="header__nav-lang">|</p>
+                <h3 class="header__nav-lang" @click="enActive = true; ruActive=false" :class="{active: enActive}">en</h3>
             </div>
             <a href="#!" class="header__nav-signin">
                 Присоединиться
             </a>
         </div>
     </nav>
->>>>>>> e31c458dc85bf3292dbcc1087201bf8111927eed
 </template>
 
 <script>
@@ -30,13 +24,15 @@
 export default {
     data(){
         return {
+            ruActive:true,
+            enActive: false,
         }
     },
     computed: {
         getMenuList(){
             return this.$store.getters.getMenuList
         }
-    }
+    },
 }
 
 </script>
