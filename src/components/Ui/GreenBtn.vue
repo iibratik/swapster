ba8fdd444b9ce4e202070e1586af749cca2671ae
iibratik <iibragimov2417@gmail.com>
2022-08-29 18:57:53 +0500

    UI components

---
+++ src/components/Ui/GreenBtn.vue
@@ -0,0 +1,32 @@
+<template>
+    <a href="#" :class="className">
+    <slot></slot>
+    </a>
+</template>
+
+<script>
+    export default {
+        props:{
+            className:{
+                type:String,
+                default:'btn'
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .btn{
+    padding: 13px 37px;
+    color: $white;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 150%;
+
+    }
+    .btn-green{
+        border: 2px solid #07DBB4;
+        filter: drop-shadow(0px 0px 11px #45AFC7);
+        border-radius: 15px;
+    }
+</style>
