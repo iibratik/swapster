e79f1ba0905f5acceb5e823f43d1c03d76ab7931
iibratik <iibragimov2417@gmail.com>
2022-09-17 23:26:52 +0500

    nav-fix

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,49 +3,66 @@
         <div class="header__nav-logo">
             <a href="#!"><img src="@/assets/images/logo.png" alt="" class="logo"></a>
         </div>
-        <ul class="header__nav-menu">
-            <li v-for="link in getMenuList" :key="link.id"><a href="#!" class="header__nav-link">{{link.title}}</a></li>
-        </ul>
-        <div class="header__nav-block">
-            <div class="header__nav-translation">
-                <h3 class="header__nav-lang" @click="ruActive = true; enActive=false" :class="{active: ruActive}">ru</h3>
-                <p class="header__nav-lang">|</p>
-                <h3 class="header__nav-lang" @click="enActive = true; ruActive=false" :class="{active: enActive}">en</h3>
-            </div>
-            <a href="#!" class="header__nav-signin">
-                Присоединиться
-            </a>
-        </div>
-        <button class="header__nav-btn" @click="burger = !burger"><img src="@/assets/images/icons/menu.svg" alt=""></button>
-        <div class="header__nav-burger" :class="{active: burger}">
-            <ul class="header__nav-menu nav-burger">
+            <ul class="header__nav-menu">
                 <li v-for="link in getMenuList" :key="link.id"><a href="#!" class="header__nav-link">{{link.title}}</a></li>
             </ul>
-        <a href="" class="header__nav-signin">Присоединиться</a>
-        </div>
+            <div class="header__nav-block">
+                <div class="header__nav-translation">
+                    <h3 class="header__nav-lang" @click="changeLang" :class="{active: ruActive}">ru</h3>
+                    <p class="header__nav-lang">|</p>
+                    <h3 class="header__nav-lang" @click="changeLang" :class="{active: enActive}">en</h3>
+                </div>
+                <green-btn href="#">
+                    Присоединиться
+                </green-btn>
+            </div>
+            <button class="burger"><i class="fas fa-bars"></i></button>
     </nav>
 </template>
 
 <script>
 
-import { mapGetters } from "vuex"
 
 export default {
     data(){
         return {
+            ruActive: localStorage.getItem('lang') === 'ru'? true : false,
+            enActive: localStorage.getItem('lang') === 'en'? true : false,
+
             ruActive:true,
             enActive: false,
             burger: false,
         }
     },
     computed: {
-        ...mapGetters(["getMenuList"])
+        getMenuList(){
+            return this.$store.getters.getMenuList
+        },
     },
-    created() {
+    methods:{
+        changeLang(){
+            if (localStorage.getItem('lang') == "en") {
 
+                this.ruActive = false;
+                this.enActive = true;
+                localStorage.setItem('lang', 'ru')
+            }else {
+                localStorage.setItem('lang', 'en')
+                this.enActive = false;
+                this.ruActive = true;
+
+            }
+
+        }
     },
-    methods: {
-    }
+    created(){
+            if (window.navigator.language === 'en-US') {
+                localStorage.setItem("lang","en")
+            }else if(window.navigator.language === 'ru-Ru') {
+                localStorage.setItem("lang","ru")
+            }
+        }
+
 }
 
 </script>
