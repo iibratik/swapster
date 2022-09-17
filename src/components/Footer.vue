f40f76c613003c052785c045b1872227ba1d54db
Jellinek <jellinekbraus@gmail.com>
2022-09-17 23:43:30 +0500

    Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,18 +2,58 @@
 footer.footer
     .footer__container
         .footer__up
-            img(src="@/assets/images/logo.png")
+            a.footer__up-logo
+                img(src="@/assets/images/logo.png")
             .footer__up-start
-                a.footer__up-gmail
+                a.footer__up-gmail(href="mailto:business@swapster.tech")
                     img(src="@/assets/images/icons/mail.svg")
+                    span business@swapster.tech
+                a.footer__up-btn(href="#!")
+                    span Запустить Swapster
+                    img(src="@/assets/images/icons/arrow.svg")
         .footer__middle
+            ul.footer__middle-menu
+                li(v-for="link in getFooterMenuList" :key="link.id")
+                    a.footer__middle-link(href="#!") {{link.title}}
+            .footer__middle-actives
+                h2.footer__middle-title Активы
+                ul.footer__middle-menu 
+                    li(v-for="link in getFooterMenuCurrenty" :key="link.id")
+                        a.footer__middle-link(href="#!") {{ link.title }}
+            .footer__middle-block
+                ul.footer__middle-menu 
+                    li
+                        a.footer__middle-link(href="#!") Криптовалюты
+                    li
+                        a.footer__middle-link(href="#!") Заработок криптовалют
+                    li
+                        a.footer__middle-link(href="#!") Криптобезопастность
+                    li
+                        a.footer__middle-link(href="#!") Криптоновости
+                .footer__middle-social
+                    h2.footer__middle-link Подпишитесь на нас
+                    .footer__middle-icons
+                        a.footer__middle-icon(href="#!" v-for="icon in getFooterMenuIcons" :key="icon.id")
+                            img(:src="require(`@/assets/images/icons/${icon.icon}`)")
         .footer__bottom
+            ul.footer__bottom-menu 
+                li 
+                    a.footer__bottom-link(href="#!") Поддержка
+                li 
+                    a.footer__bottom-link(href="#!") Политика конфиденциальности
+                li 
+                    a.footer__bottom-link(href="#!") Условия использования
+            p.footer__bottom-copyright  © 2022 Swapster
 </template>
 
 <script>
 
-export default {
+import {mapGetters} from "vuex";
 
+export default {
+    computed:{
+        ...mapGetters(["getFooterMenuList", "getFooterMenuCurrenty", "getFooterMenuIcons"])
+    }
 }
 
 </script>
