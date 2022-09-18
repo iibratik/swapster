0b37ec1bda149f0c8969c134bb29b23bc90e07ba
Jellinek <jellinekbraus@gmail.com>
2022-09-18 14:48:23 +0500

    Supported Assets

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,6 +2,7 @@
 <main class="main">
     <div class="main__container">
         <MainBenefits />
+        <MainSupportedAssets />
         <MainAppFunctions />
     </div>
 </main>
@@ -11,11 +12,13 @@
 
 import MainBenefits from "@/components/Main/MainBenefits.vue";
 import MainAppFunctions from "@/components/Main/MainAppFunctions.vue";
+import MainSupportedAssets from "@/components/Main/MainSupportedAssets.vue";
 
 export default {
     components: {
         MainBenefits,
-        MainAppFunctions
+        MainAppFunctions,
+        MainSupportedAssets
     }
 }
 
