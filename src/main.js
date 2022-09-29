809c58fcfdaaf3b6ec63355018308afc97987743
iibratik <iibragimov2417@gmail.com>
2022-09-29 20:41:29 +0500

    slider

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-// import router from './router'
+import router from './router'
 import store from './store'
 import "@/assets/styles/style.scss"
 import components from "@/components/Ui/UIscript";
